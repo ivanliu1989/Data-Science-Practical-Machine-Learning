45639161ce2c137552b57e5e65c5f27c04e66ab6
ivan-liuyanfeng <ivan.liuyanfeng@gmail.com>
2014-09-03 08:35:08 +1000

    wk1

--- wk1.R
+++ wk1.R
@@ -2,6 +2,10 @@ setwd("C:\\Users\\Ivan.Liuyanfeng\\Desktop\\Data_Mining_Work_Space\\Data-Science
 library(kernlab)
 data(spam)
 head(spam)
-
+png('spam.png')
 plot(density(spam$your[spam$type=='nonspam']),col='blue',main='',xlab='Frequency of "Your"')
 lines(density(spam$your[spam$type=='spam']),col='red')
+abline(v=.5, col='black')
+dev.off()
+prediction <- ifelse(spam$your>0.5,'spam','nonspam')
+table(prediction,spam$type)/length(spam$type)
