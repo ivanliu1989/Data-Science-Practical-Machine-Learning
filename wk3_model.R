bebb5070cd605128009a00286b0b051f5b9d94e6
ivan-liuyanfeng <ivan.liuyanfeng@gmail.com>
2014-09-04 10:17:53 +1000

    wk3

---
+++ wk3_model.R
@@ -0,0 +1,18 @@
+setwd('C:\\Users\\Ivan.Liuyanfeng\\Desktop\\Data_Mining_Work_Space\\Data-Science-Practical-Machine-Learning')
+data(iris); library(ggplot2)
+names(iris)
+table(iris$Species)
+inTrain <- createDataPartition(y=iris$Species,
+                               p=0.7, list=FALSE)
+training <- iris[inTrain,]
+testing <- iris[-inTrain,]
+dim(training); dim(testing)
+modlda = train(Species ~ .,data=training,method="lda")
+modnb = train(Species ~ ., data=training,method="nb")
+plda = predict(modlda,testing); pnb = predict(modnb,testing)
+table(plda,pnb)
+
+equalPredictions = (plda==pnb)
+png('modelbased.png')
+qplot(Petal.Width,Sepal.Width,colour=equalPredictions,data=testing)
+dev.off()
