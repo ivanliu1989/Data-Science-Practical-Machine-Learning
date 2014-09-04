758030d1175a624d47b76e0a252cca6b9ace2c72
ivan-liuyanfeng <ivan.liuyanfeng@gmail.com>
2014-09-04 14:40:59 +1000

    wk4

---
+++ wk4_forecasting.R
@@ -0,0 +1,27 @@
+setwd("C:\\Users\\Ivan.Liuyanfeng\\Desktop\\Data_Mining_Work_Space\\Data-Science-Practical-Machine-Learning")
+library(quantmod)
+from.dat <- as.Date("01/01/08", format="%m/%d/%y")
+to.dat <- as.Date("12/31/13", format="%m/%d/%y")
+getSymbols("GOOG", src="google", from = from.dat, to = to.dat)
+head(GOOG)
+## Summarize monthly and store as time series
+mGoog <- to.monthly(GOOG)
+googOpen <- Op(mGoog)
+ts1 <- ts(googOpen,frequency=12)
+plot(ts1,xlab="Years+1", ylab="GOOG")
+?to.monthly
+## Decompose a time series into parts
+plot(decompose(ts1),xlab="Years+1")
+## Training and test sets
+ts1Train <- window(ts1,start=1,end=5)
+ts1Test <- window(ts1,start=5,end=(7-0.01))
+ts1Train
+## Simple moving average
+plot(ts1Train)
+lines(ma(ts1Train,order=3),col="red")
+## Exponential smoothing
+ets1 <- ets(ts1Train,model="MMM")
+fcast <- forecast(ets1)
+plot(fcast); lines(ts1Test,col="red")
+## Get the accuracy
+accuracy(fcast,ts1Test)
